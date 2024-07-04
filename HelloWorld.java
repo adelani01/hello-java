d29f8de806bb291f28325e284353ce48f3d9a665
Adelani Emmanuel Damilare <adelanidamilare@gmail.com>
2024-07-04 13:19:53 +0000

    added first hello java file

---
+++ HelloWorld.java
@@ -0,0 +1,7 @@
+public class HelloWorld {
+  public static void main(String[] args) {
+    System.out.println("Hello, World");
+    System.out.println("This is emmanuel");
+    System.out.println("HI 43 Batch devops");
+  }
+}
