f09abbfd455dd489b85e24eb08d50a3957060d3d
ADELANI EMMANUEL DAMILARE <71339742+adelani01@users.noreply.github.com>
2024-07-06 20:26:46 +0100

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -11,10 +11,7 @@ public class HelloWorld { public static void main(String[] args) { System.out.pr
     System.out.println("Hello, World");
     System.out.println("Hello, World");
     System.out.println("Hello, World");
-    System.out.println("Hello, World");
-    System.out.println("Hello, World");
-    System.out.println("Hello, World");
-    System.out.println("Hello, World");
+    
     System.out.println("HI 43 Batch devops");
     System.out.println("HI 43 Batch devops");
     System.out.println("HI 43 Batch devops");
