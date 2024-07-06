7d4adccb3ca9ed5bcc4aa94e6a6f563b35d76993
Adelani Emmanuel Damilare <adelanidamilare@gmail.com>
2024-07-06 18:06:10 +0000

    more added

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,12 +1,16 @@
-public class HelloWorld {
-  public static void main(String[] args) {
-    System.out.println("Hello, World");
-    System.out.println("This is emmanuel");
+public class HelloWorld { public static void main(String[] args) { System.out.println("Hello, World"); System.out.println("This is emmanuel");
     System.out.println("Hello, World");
     System.out.println("Hello, World");
     System.out.println("Hello, World");
     System.out.println("Hello, World");
     System.out.println("Hello, World");
+    System.out.println("HI 43 Batch devops");
+    System.out.println("HI 43 Batch devops");
+    System.out.println("HI 43 Batch devops");
+    System.out.println("HI 43 Batch devops");
+    System.out.println("HI 43 Batch devops");
+    System.out.println("HI 43 Batch devops");
+    System.out.println("HI 43 Batch devops");
     System.out.println("HI 43 Batch devops");
   }
 }
